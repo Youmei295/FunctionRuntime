cebc34f13a5b1c5feca258c8b8ea63f12855665e
Tran Tan Phat <trantanphat.stu@gmail.com>
2025-04-25 21:05:43 +0700

    Remove some unnecessary parts

--- Measure_Time.cpp
+++ Measure_Time.cpp
@@ -15,16 +15,12 @@ int main()
 	vector<int> arr1 = randomarray(n, 1500000000), arr2 = nearlysorted(n, 1500000000), arr3 = reversesort(n, 1500000000), arr4 = sorted(n, 1500000000);
 	double time1 = measureExecutionTime(heapsort,arr1,n);
 	if (checksorted(arr1, 0, n - 1)) cout << "1: " << time1 << endl;
-	delete[] arr1;
 	double time2 = measureExecutionTime(heapsort,arr2,n);
 	if (checksorted(arr2, 0, n - 1)) cout << "2: " << time2 << endl;
-	delete[] arr2;
 	double time3 = measureExecutionTime(heapsort, arr3, n);
 	if (checksorted(arr3, 0, n - 1)) cout << "3: " << time3 << endl;
-	delete[] arr3;
 	double time4 = measureExecutionTime(heapsort, arr4, n);
 	if (checksorted(arr4, 0, n - 1)) cout << "4: " << time4 << endl;
-	delete[] arr4;
 }
 
 
